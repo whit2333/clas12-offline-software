64ee5eef349051fa92a2dd97a7e77f89070309c8
David Heddle <dheddle@circadence.com>
2018-06-25 15:08:43 -0400

    final changes fastMCed build 0.5

--- common-tools/cnuphys/fastmCED/fastmced.sh
+++ common-tools/cnuphys/fastmCED/fastmced.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-SCRIPT_DIR=`dirname $0`
-
-echo $SCRIPT_DIR
-JARNAME=$SCRIPT_DIR/fastmced.jar
-MAIN=cnuphys.ced.frame.FatsMCed
+JARNAME=./fastmced.jar
+MAIN=cnuphys.ced.frame.FastMCed
 VARGS="-Dsun.java2d.pmoffscreen=false -Xmx1024M -Xss512k"
 java $VARGS -jar $JARNAME $MAIN
