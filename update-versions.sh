924bd8dd57ef9a2a6f0b0d8752d509da66eaf927
baltzell <baltzell@gmx.com>
2018-12-02 13:09:24 -0500

    common-tools only on version updater

--- update-versions.sh
+++ update-versions.sh
@@ -3,7 +3,13 @@
 #newVersion=$1
 newVersion=$1-SNAPSHOT
 
-mvn --batch-mode release:update-versions -DdevelopmentVersion=$newVersion
+batch="mvn --batch-mode release:update-versions -DdevelopmentVersion=$newVersion"
+interactive="mvn release:update-versions"
+ 
+cd common-tools
+$batch
+cd -
 
 sed -i '' "s/coat-libs-\(.*\)\.jar/coat-libs-$newVersion.jar/" ./build-coatjava.sh
+sed -i '' "s/coat-libs-\(.*\)\.jar/coat-libs-$newVersion.jar/" ./common-tools/coat-lib/deployDistribution.sh
 
