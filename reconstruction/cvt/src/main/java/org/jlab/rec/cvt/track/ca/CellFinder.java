923e02716d28b67085ca3b9c41dd9076590b2552
ziegler <ziegler@jlab.org>
2018-12-19 17:25:43 -0500

    New tools package for CA.  Can be extended to other ML algorithms. Adding cell/node methods.

--- reconstruction/cvt/src/main/java/org/jlab/rec/cvt/track/ca/CellFinder.java
+++ reconstruction/cvt/src/main/java/org/jlab/rec/cvt/track/ca/CellFinder.java
@@ -10,7 +10,9 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.jlab.clas.clas.mla.ca.cvt.Cell;
 import org.jlab.clas.clas.mla.ca.cvt.Node;
+import org.jlab.rec.cvt.bmt.Geometry;
 import org.jlab.rec.cvt.cross.Cross;
 
 /**
@@ -24,6 +26,8 @@ public class CellFinder {
     int[] sinkReg   = {2,3,3,4,4,5,5,5,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9};
     
     private Map<Integer, Node> nodesFromCrosses = null;
+    private List<Cell> cells = null;
+    
     private void getNodes(List<Cross> crosses) {
         nodesFromCrosses = new HashMap<Integer, Node>();
         Collections.sort(crosses);
@@ -35,7 +39,9 @@ public class CellFinder {
                 Node nodet = new Node(c.get_Point(), radius, Node.DetectorType.SVTT);
                 Node nodel = new Node(c.get_Point(), radius, Node.DetectorType.SVTL);
                 nodet.setRegion(c.get_Region());
+                nodet.setSector(c.get_Sector());
                 nodel.setRegion(c.get_Region());
+                nodel.setSector(c.get_Sector());               
                 nodesFromCrosses.put(nodet.getRegion(), nodet);
                 nodesFromCrosses.put(nodel.getRegion(), nodel);
             }
@@ -46,25 +52,57 @@ public class CellFinder {
                     double radius = Math.sqrt(x*x + y*y);
                     Node node = new Node(c.get_Point(), radius, Node.DetectorType.BMTZ);
                     node.setRegion(3 + lZ[c.get_Region()-1]);
+                    node.setSector(c.get_Sector());     
                     nodesFromCrosses.put(node.getRegion(), node);
                 } 
                 if(c.get_DetectorType().equalsIgnoreCase("C")) {
                     double radius = org.jlab.rec.cvt.bmt.Constants.getCRCRADIUS()[c.get_Region()-1];
                     Node node = new Node(c.get_Point(), radius, Node.DetectorType.BMTC);
                     node.setRegion(3 + lC[c.get_Region()-1]);
+                    node.setSector(c.get_Sector());     
                     nodesFromCrosses.put(node.getRegion(), node);
                 } 
             }
         }
     }
     
-    private void getCells() {
+    private void getCells(Geometry bgeom) {
+        cells = new ArrayList<Cell>();  
         for(int i = 0; i < sourceReg.length; i++) {
             if(nodesFromCrosses.containsKey(sourceReg[i]) && 
                     nodesFromCrosses.containsKey(sinkReg[i])) {
-                
+                if(nodesFromCrosses.get(sinkReg[i]).getRegion()>3) { // stay in the same BMT sector
+                    if(nodesFromCrosses.get(sourceReg[i]).getRegion()>3) {
+                        if(nodesFromCrosses.get(sinkReg[i]).getSector()!=nodesFromCrosses.get(sourceReg[i]).getSector())
+                            continue;
+                    } else {
+                        if( ! bgeom.checkIsInSector( nodesFromCrosses.get(sourceReg[i]).getPoint().toVector3D().phi(), 
+                                nodesFromCrosses.get(sinkReg[i]).getSector(), 1, Math.toRadians(10) )  )
+                            continue;
+                    }
+                }
+                Cell cell = new Cell(nodesFromCrosses.get(sourceReg[i]),
+                    nodesFromCrosses.get(sinkReg[i]));
+                if(cell.getPath()!=null)
+                    getCells().add(cell);
             }
         }
+    }
+    
+    public void findCells(List<Cross> crosses, Geometry bgeom) {
+        this.getNodes(crosses);
+        if(this.nodesFromCrosses==null) {
+            return;
+        } else {
+            this.getCells(bgeom);
+        }
+    }
+
+    /**
+     * @return the cells
+     */
+    public List<Cell> getCells() {
+        return cells;
     }
     
 }
